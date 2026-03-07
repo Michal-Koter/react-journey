fc5d111ef7321d30360a4188ebee80293826b4a2
Michal-Koter <kotermichal@outlook.com>
2026-03-07 13:26:12 +0100

    states and rendering content conditionally

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,9 +1,17 @@
-import {CORE_CONCEPTS} from "./data";
+import {useState} from "react";
+
+import {CORE_CONCEPTS, EXAMPLES} from "./data";
 import CoreConcept from "./components/CoreConcepts/CoreConcept";
 import Header from "./components/Header/Header";
 import TabButton from "./components/TabButton/TabButton";
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    function handleSelect(selectedButton) {
+        setSelectedTopic(selectedButton);
+    }
+
     return (
         <div>
             <Header/>
@@ -20,11 +28,23 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton>Components</TabButton>
-                        <TabButton>JSX</TabButton>
-                        <TabButton>Props</TabButton>
-                        <TabButton>State</TabButton>
+                        <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
                     </menu>
+
+                    {!selectedTopic ? (
+                        <p>Pleas select a topic.</p>
+                    ) : (
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description}</p>
+                            <pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                            </pre>
+                        </div>
+                    )}
                 </section>
             </main>
         </div>
