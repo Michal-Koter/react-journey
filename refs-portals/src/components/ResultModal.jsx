4dacbce45850de820d3777004a4d90d1cba04ec6
Michal-Koter <kotermichal@outlook.com>
2026-03-22 15:03:37 +0100

    calculate score

--- refs-portals/src/components/ResultModal.jsx
+++ refs-portals/src/components/ResultModal.jsx
@@ -23,8 +23,12 @@ import {forwardRef, useImperativeHandle, useRef} from "react";
 /**
  *  NOTE: this is a newer way to forward ref in React >= 19
  */
-export default function ResultModal({ref, result, targetTime}) {
+export default function ResultModal({ref, targetTime, remainingTime, onReset}) {
     const dialog = useRef();
+
+    const userLost = remainingTime <= 0
+        , formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+        , score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return {
@@ -35,11 +39,12 @@ export default function ResultModal({ref, result, targetTime}) {
     });
 
     return (
-        <dialog ref={dialog} className="result-modal">
-            <h2>You {result}</h2>
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+            {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
