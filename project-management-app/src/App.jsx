abef9779014933405ce37d4842bb268334d43241
Michal-Koter <kotermichal@outlook.com>
2026-03-30 21:15:28 +0200

    update sidebar style

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -2,10 +2,10 @@ import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
   return (
-    <div className="mt-8 grid grid-cols-5 gap-4">
+    <main className="h-sreen my-8">
         <Sidebar/>
         <h1 className="col-span-4 my-8 text-center text-5xl font-bold">Hello World</h1>
-    </div>
+    </main>
   );
 }
 
--- project-management-app/src/components/Sidebar.jsx
+++ project-management-app/src/components/Sidebar.jsx
@@ -1,14 +1,16 @@
 export default function Sidebar(props) {
     return (
-        <section className="p-4 flex-col h-screen bg-black">
-            <h2 className="ml-8 mt-12 text-2xl font-bold text-white capitalize">Your Projects</h2>
-            <button className="ml-8 my-8 px-3 py-2 rounded-lg bg-mauve-500 hover:bg-mauve-700">
-                + Add Project
-            </button>
-            <div className="ml-8">
-                {/* placeholder for dynamic added tabs */}
-                <p className="mt-4 mx-2 text-stone-400">Template project</p>
+        <aside className="w-1/3 px-8 py-16 bg-black md:w-72 rounded-r-xl">
+            <h2 className="mb-8 mt-12  font-bold text-stone-100 capitalize md:text-2xl">Your Projects</h2>
+            <div>
+                <button className="px-4 py-2 rounded-lg text-xs md:text-base text-stone-300 hover:text-stone-200 bg-mauve-700 hover:bg-mauve-500">
+                    + Add Project
+                </button>
             </div>
-        </section>
+            <ul className="ml-8">
+                {/* placeholder for dynamic added tabs */}
+                <li className="mt-4 mx-2 text-stone-400">Template project</li>
+            </ul>
+        </aside>
     )
 }
