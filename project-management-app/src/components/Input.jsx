dff04a3f5f5d6f5c8ce969b7b3519293965e1d93
Michal-Koter <kotermichal@outlook.com>
2026-03-31 20:43:35 +0200

    add NewProject component styling

--- project-management-app/src/components/Input.jsx
+++ project-management-app/src/components/Input.jsx
@@ -1,10 +1,11 @@
 export default function Input({label, isTextarea = false, ...props}) {
+    const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-ston-600 focus:outline-none focus:border-stone-600";
     return (
-        <div>
-            <label>{label}</label>
+        <div className="flex flex-col gap-1 my-4">
+            <label className="text-sm font-bold uppercase text-stone-400">{label}</label>
             {isTextarea
-                ? <textarea {...props}/>
-                : <input {...props}/>
+                ? <textarea className={classes} {...props}/>
+                : <input className={classes} {...props}/>
             }
         </div>
     )
--- project-management-app/src/components/NewProject.jsx
+++ project-management-app/src/components/NewProject.jsx
@@ -2,13 +2,13 @@ import Input from "./Input.jsx";
 
 export default function NewProject() {
     return (
-        <div>
-            <menu>
+        <div className="h-screen w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
                 <li>
-                    <button>Cancel</button>
+                    <button className="text-stone-700 hover:text-stone-900">Cancel</button>
                 </li>
                 <li>
-                    <button>Save</button>
+                    <button className="px-6 py-2 rounded-md bg-stone-600 text-stone-100 hover:bg-stone:900">Save</button>
                 </li>
             </menu>
             <div>
