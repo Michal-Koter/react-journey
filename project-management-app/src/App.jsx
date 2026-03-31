83c8b4bf569260dab46976fc8230d02524a38ea1
Michal-Koter <kotermichal@outlook.com>
2026-03-31 21:10:16 +0200

    dynamic context change

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,14 +1,32 @@
 import Sidebar from "./components/Sidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
 
 function App() {
-  return (
-    <main className="h-sreen my-8 flex gap-8">
-        <Sidebar/>
-        <NoProjectSelected/>
-    </main>
-  );
+    const [projects, setProjects] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    });
+
+    function handleStartAddProject() {
+        setProjects(prev => ({
+            ...prev,
+            selectedProjectId: null
+        }));
+    }
+
+    return (
+        <main className="h-sreen my-8 flex gap-8">
+            <Sidebar handleStartAddProject={handleStartAddProject}/>
+            {projects.selectedProjectId === undefined
+                ? <NoProjectSelected handleStartAddProject={handleStartAddProject}/>
+                : projects.selectedProjectId === null
+                    ? <NewProject/>
+                    : null //TODO: add in the future project component
+            }
+        </main>
+    );
 }
 
 export default App;
--- project-management-app/src/components/Sidebar.jsx
+++ project-management-app/src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
 import Button from "./Button"
 
-export default function Sidebar(props) {
+export default function Sidebar({handleStartAddProject}) {
     return (
         <aside className="h-screen w-1/3 px-8 py-16 bg-black md:w-72 rounded-r-xl">
             <h2 className="mb-8 mt-12  font-bold text-stone-100 capitalize md:text-2xl">Your Projects</h2>
             <div>
-                <Button>+ Add Project</Button>
+                <Button onClick={handleStartAddProject}>+ Add Project</Button>
             </div>
             <ul className="ml-8">
                 {/* placeholder for dynamic added tabs */}
