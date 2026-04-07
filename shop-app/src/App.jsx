1bba8bad532dc55f7b9e076fb273bd4b9338a985
Michal-Koter <kotermichal@outlook.com>
2026-04-07 20:00:04 +0200

    create, provide and consume context

--- shop-app/src/App.jsx
+++ shop-app/src/App.jsx
@@ -4,6 +4,7 @@ import Product from './components/Product.jsx';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import {CartContext} from "./store/shopping-cart-context.jsx"
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -66,8 +67,14 @@ function App() {
     });
   }
 
+  const ctxValue = {
+      items: shoppingCart.items,
+      addItemToCart: handleAddItemToCart,
+
+  }
+
   return (
-    <>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -75,11 +82,11 @@ function App() {
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
             <li key={product.id}>
-              <Product {...product} onAddToCart={handleAddItemToCart} />
+              <Product {...product}/>
             </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
