fedd8583a826d82d0dd992d99ea60da94d8c416b
Michal-Koter <kotermichal@outlook.com>
2026-04-08 18:57:37 +0200

    replace prop drilling by context

--- shop-app/src/components/Cart.jsx
+++ shop-app/src/components/Cart.jsx
@@ -5,7 +5,7 @@
 
 import {CartContext} from "../store/shopping-cart-context.jsx"
 
-export default function Cart({onUpdateItemQuantity}) {
+export default function Cart() {
     // const cartCtx = useContext(CartContext)
 
     return (
@@ -32,11 +32,11 @@ export default function Cart({onUpdateItemQuantity}) {
                                                 <span> ({formattedPrice})</span>
                                             </div>
                                             <div className="cart-item-actions">
-                                                <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                                                <button onClick={() => cartCtx.updateCartItemQuantity(item.id, -1)}>
                                                     -
                                                 </button>
                                                 <span>{item.quantity}</span>
-                                                <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                                                <button onClick={() => cartCtx.updateCartItemQuantity(item.id, 1)}>
                                                     +
                                                 </button>
                                             </div>
