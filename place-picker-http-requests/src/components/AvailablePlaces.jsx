e34e34b2447464a20bcfde2ab80bfa1525112a82
Michal-Koter <kotermichal@outlook.com>
2026-04-18 10:06:29 +0200

    fetch places data

--- place-picker-http-requests/src/components/AvailablePlaces.jsx
+++ place-picker-http-requests/src/components/AvailablePlaces.jsx
@@ -1,10 +1,28 @@
 import Places from './Places.jsx';
+import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    // async way of fetching data
+    // async function fetchPlaces() {
+    //   const response = await fetch('http://localhost:3000/places');
+    //   const resData = await response.json();
+    //   setAvailablePlaces(resData.places);
+    // }
+    // fetchPlaces();
+
+    // fetch data with Promise
+    fetch('http://localhost:3000/places')
+        .then(res => res.json())
+        .then(data => setAvailablePlaces(data.places));
+  }, [])
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
