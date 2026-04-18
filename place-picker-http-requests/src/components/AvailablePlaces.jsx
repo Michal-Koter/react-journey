ca1f220a27761408abc7aacd5cac04400f888b33
Michal-Koter <kotermichal@outlook.com>
2026-04-18 10:12:17 +0200

    add info about loading data

--- place-picker-http-requests/src/components/AvailablePlaces.jsx
+++ place-picker-http-requests/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import Places from './Places.jsx';
 import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
@@ -14,15 +15,21 @@ export default function AvailablePlaces({ onSelectPlace }) {
     // fetchPlaces();
 
     // fetch data with Promise
+    setIsFetching(true);
     fetch('http://localhost:3000/places')
         .then(res => res.json())
-        .then(data => setAvailablePlaces(data.places));
+        .then(data => {
+          setAvailablePlaces(data.places);
+          setIsFetching(false);
+        });
   }, [])
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
