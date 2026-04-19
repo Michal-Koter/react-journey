5cbd52c537adfc027a0e0dc2c4c10293546efbb7
Michal-Koter <kotermichal@outlook.com>
2026-04-19 10:13:45 +0200

    add sort places by distance

--- place-picker-http-requests/src/components/AvailablePlaces.jsx
+++ place-picker-http-requests/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import Places from './Places.jsx';
 import {useEffect, useState} from "react";
 import Error from "./Error.jsx";
+import {sortPlacesByDistance} from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -20,15 +21,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
     setIsFetching(true);
     fetch('http://localhost:3000/places')
         .then(res => {
-            if (res.ok) return res.json()
+            if (res.ok) return res.json();
             throw new Error('Failed to fetch place data.');
         })
         .then(data => {
-          setAvailablePlaces(data.places);
-          setIsFetching(false);
+            navigator.geolocation.getCurrentPosition(position => {
+                const sortedPlaces = sortPlacesByDistance(data.places, position.coords.latitude, position.coords.longitude);
+                setAvailablePlaces(sortedPlaces);
+                setIsFetching(false);
+            })
         })
         .catch(error => {
             setError({message: error.message || "Could not fetch places, please try again later."});
+            setIsFetching(false);
         });
   }, [])
 
