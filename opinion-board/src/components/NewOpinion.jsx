b3bd16a86084dff76686b78e1cd8eaab99718b04
Michal-Koter <kotermichal@outlook.com>
2026-05-14 20:43:38 +0200

    add useFormStatus to update UI during request

--- opinion-board/src/components/NewOpinion.jsx
+++ opinion-board/src/components/NewOpinion.jsx
@@ -1,38 +1,42 @@
-import {useActionState} from "react";
-
-function createOpinionAction(prevFormState, formData) {
-  const userName = formData.get('userName');
-  const title = formData.get('title');
-  const body = formData.get('body');
-
-  let errors = [];
-  if (!userName.trim()) {
-    errors.push("Name is required.");
-  }
-  if (title.trim().length < 5) {
-    errors.push("Title must be at least 5 characters.");
-  }
-  if (body.trim().length < 10 || body.trim().length > 300) {
-    errors.push("Opinion must be between 10 and 300 characters.");
-  }
-  if (errors.length > 0) {
-    return {
-      errors: errors,
-      opinion: {
-        userName,
-        title,
-        body,
-      }
-    }
-  }
-
-  // submit to backend
-
-  return {errors: null}
-}
+import {useActionState, use} from "react";
+import {OpinionsContext} from "../store/opinions-context.jsx";
+import Submit from "./Submit.jsx";
 
 export function NewOpinion() {
+  const {addOpinion} = use(OpinionsContext);
+
   const [formState, formAction] = useActionState(createOpinionAction, {error: null});
+
+  async function createOpinionAction(prevFormState, formData) {
+    const userName = formData.get('userName');
+    const title = formData.get('title');
+    const body = formData.get('body');
+
+    let errors = [];
+    if (!userName.trim()) {
+      errors.push("Name is required.");
+    }
+    if (title.trim().length < 5) {
+      errors.push("Title must be at least 5 characters.");
+    }
+    if (body.trim().length < 10 || body.trim().length > 300) {
+      errors.push("Opinion must be between 10 and 300 characters.");
+    }
+    if (errors.length > 0) {
+      return {
+        errors: errors,
+        opinion: {
+          userName,
+          title,
+          body,
+        }
+      }
+    }
+
+    await addOpinion({title, body, userName});
+
+    return {errors: null}
+  }
 
   return (
     <div id="new-opinion">
@@ -60,10 +64,7 @@ export function NewOpinion() {
             </ul>
         )}
 
-
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+      <Submit />
       </form>
     </div>
   );
