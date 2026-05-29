fbe27807252ab862f472a7a636112aaac4397fe5
Michal-Koter <kotermichal@outlook.com>
2026-05-29 19:29:00 +0200

    add cart and ui redux to redux-cart

--- redux-cart/src/components/Shop/Products.js
+++ redux-cart/src/components/Shop/Products.js
@@ -1,19 +1,44 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+    {
+        id: "p1"
+        , price: 6
+        , title: "Product 2"
+        , description: "Product 1 description"
+    }
+    , {
+        id: "p2"
+        , price: 2
+        , title: "Product 2"
+        , description: "Product 2 description"
+    }
+    , {
+        id: "p3"
+        , price: 7
+        , title: "Product 3"
+        , description: "Product 3 description"
+    }
+]
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map(product => (
+                    <ProductItem
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        price={product.price}
+                        description={product.description}
+                    />
+                ))}
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
