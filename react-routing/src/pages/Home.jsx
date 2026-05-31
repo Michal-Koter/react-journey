0e1329fd0a861912ac113cbe5533ea93d643a53d
Michal-Koter <kotermichal@outlook.com>
2026-05-31 13:41:05 +0200

    add button to navigate programmatically

--- react-routing/src/pages/Home.jsx
+++ react-routing/src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function HomePage() {
+    const navigate = useNavigate();
+
+    function handleNavigate() {
+        navigate("/products");
+    }
+
     return (
         <>
             <h1>My home page.</h1>
             <p>Go to <Link to="/products">the list of products</Link>.</p>
-        </>)
+            <p><button onClick={handleNavigate}>Navigate</button></p>
+        </>
+    )
 }
