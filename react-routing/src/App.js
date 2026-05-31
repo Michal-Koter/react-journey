cd189a478a492023a811f8718273108875959447
Michal-Koter <kotermichal@outlook.com>
2026-05-31 13:21:06 +0200

    add custor error page

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -2,6 +2,7 @@ import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} fr
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/Error";
 
 // Note: older way of creating routes
 // const routeDefinitions = createRoutesFromElements(
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
     {
         path: "/"
         , element: <RootLayout/>
+        , errorElement: <ErrorPage/>
         , children: [
             {path: "/", element: <HomePage/>}
             , {path: "/products", element: <ProductsPage/>}
