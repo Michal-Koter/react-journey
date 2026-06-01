5fc02faa4bdd16d1445e245b7b2206737ac7afed
Michal-Koter <kotermichal@outlook.com>
2026-06-01 20:32:44 +0200

    create root element with navigation bar

--- events-app/frontend/src/App.js
+++ events-app/frontend/src/App.js
@@ -4,11 +4,9 @@ import EventsPage from "./page/Events";
 import EventDetailPage from "./page/EventDetail";
 import NewEventPage from "./page/NewEvent";
 import EditEventPage from "./page/EditEvent";
+import RootLayout from "./page/RootLayout";
 // Challenge / Exercise
 
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
@@ -18,24 +16,31 @@ import EditEventPage from "./page/EditEvent";
 const router = createBrowserRouter([
     {
         path: "/"
-        , element: <HomePage/>
+        , element: <RootLayout/>
+        , children: [
+            {
+                index: true
+                , element: <HomePage/>
+            }
+            , {
+                path: "events"
+                , element: <EventsPage/>
+            }
+            , {
+                path: "events/:eventId"
+                , element: <EventDetailPage/>
+            }
+            , {
+                path: "events/new"
+                , element: <NewEventPage/>
+            }
+            , {
+                path: "events/:eventId/edit"
+                , element: <EditEventPage/>
+            }
+        ]
     }
-    , {
-        path: "/events"
-        , element: <EventsPage/>
-    }
-    , {
-        path: "/events/:eventId"
-        , element: <EventDetailPage/>
-    }
-    , {
-        path: "/events/new"
-        , element: <NewEventPage/>
-    }
-    , {
-        path: "/events/:eventId/edit"
-        , element: <EditEventPage/>
-    }
+
 ]);
 
 function App() {
