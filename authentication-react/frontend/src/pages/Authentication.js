4efae0367a3a9fea999e85000f859f25a0ff945b
Michal-Koter <kotermichal@outlook.com>
2026-06-08 18:38:02 +0200

    fix: check response status

--- authentication-react/frontend/src/pages/Authentication.js
+++ authentication-react/frontend/src/pages/Authentication.js
@@ -29,7 +29,7 @@ export async function action({request}) {
     body: JSON.stringify(authData)
   })
 
-  if (request.status === 422 || request.status === 401) {
+  if (response.status === 422 || response.status === 401) {
     return response;
   }
 
