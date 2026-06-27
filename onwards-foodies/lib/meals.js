ad55128800f1b9453fdff75606162e13098d9212
Michal-Koter <kotermichal@outlook.com>
2026-06-27 15:45:14 +0200

    handle error and not found with next.js

--- onwards-foodies/lib/meals.js
+++ onwards-foodies/lib/meals.js
@@ -5,5 +5,7 @@ const db = sql('meals.db');
 export async function getMeals() {
     // promise to simulate a delay
     await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // throw new Error("Loading meals failed!");
     return db.prepare('SELECT * FROM meals').all();
 }
