559651ec8d18c02b833ded1ed02315d574254816
Michal-Koter <kotermichal@outlook.com>
2026-06-27 15:59:39 +0200

    add details meal page

--- onwards-foodies/app/meals/[mealSlug]/page.js
+++ onwards-foodies/app/meals/[mealSlug]/page.js
@@ -1,4 +1,30 @@
+import classes from './page.module.css';
+import Image from 'next/image';
+import {getMealBySlug} from "@/lib/meals";
+
 export default async function MealPage({params}) {
     const {mealSlug} = await params;
-    return (<h1>{mealSlug} Meal Page</h1>)
+    const meal = getMealBySlug(mealSlug);
+
+    return (
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill/>
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions
+                }}></p>
+            </main>
+        </>
+    );
 }
