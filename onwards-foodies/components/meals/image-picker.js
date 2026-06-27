553c45b856cbd21d5fc4df595f3f22a7e2a20499
Michal-Koter <kotermichal@outlook.com>
2026-06-28 01:22:26 +0200

    add share page with custom image picker

---
+++ onwards-foodies/components/meals/image-picker.js
@@ -0,0 +1,64 @@
+"use client";
+
+import classes from './image-picker.module.css';
+import {useRef, useState} from "react";
+import Image from "next/image";
+
+export default function ImagePicker({label, name}) {
+    const [pickedImage, setPickedImage] = useState();
+    const imageInputRef = useRef();
+
+    function handleClick() {
+        imageInputRef.current.click();
+    }
+
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            setPickedImage(reader.result);
+        };
+        reader.readAsDataURL(file);
+    }
+
+    return (
+        <div className={classes.picker}>
+            <label htmlFor={name}>{label}</label>
+            <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage && (
+                        <Image 
+                            src={pickedImage} 
+                            alt="The image selected by user." 
+                            fill/>
+                    )}
+                </div>
+                <input
+                    className={classes.input}
+                    type="file"
+                    id={name}
+                    accept="image/png, image/jpg"
+                    name={name}
+                    ref={imageInputRef}
+                    onChange={handleImageChange}
+                    required
+                />
+                <button
+                    className={classes.button}
+                    type="button"
+                    onClick={handleClick}
+                >
+                    Pick a Image
+                </button>
+            </div>
+        </div>
+    );
+}
