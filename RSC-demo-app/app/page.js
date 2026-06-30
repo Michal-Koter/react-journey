de79c1a503eaceb71c738250daa19ae5d125bb08
Michal-Koter <kotermichal@outlook.com>
2026-06-30 20:27:43 +0200

    discover the possibilities of server-side components

--- RSC-demo-app/app/page.js
+++ RSC-demo-app/app/page.js
@@ -1,12 +1,36 @@
+import {Suspense} from "react";
+import fs from "node:fs";
+
 import RSCDemo from "@/components/RSCDemo";
 import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
+import ServerActionDemo from "@/components/ServerActionDemo";
+import UsePromiseDemo from "@/components/UsePromisesDemo";
+import ErrorBoundary from "@/components/ErrorBoundery";
 
-export default function Home() {
+export default async function Home() {
+    const fetchUsersPromise = new Promise((resolve, reject) => {
+        const data = fs.readFileSync('dummy-db.json', 'utf-8');
+        const users = JSON.parse(data);
+        resolve(users);
+        // reject(new Error("Failed to load users"));
+    }, 2000);
+
     return (
         <main>
+            {/*
             <ClientDemo >
                 <RSCDemo/>
             </ClientDemo>
+            */}
+            {/*<DataFetchingDemo />*/}
+            {/*<ServerActionDemo />*/}
+            <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <UsePromiseDemo usersPromise={fetchUsersPromise} />
+                </Suspense>
+            </ErrorBoundary>
+
         </main>
     );
 }
